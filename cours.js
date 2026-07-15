f066267333f5e4c20e2e2ee02e271f665facdadc
DevSandrine <149612201+DevSandrine@users.noreply.github.com>
2026-07-15 21:20:01 +0200

    Create cours.js

---
+++ cours.js
@@ -0,0 +1,38 @@
+const cours = [
+    {
+        titre: "Introduction aux Sciences de l'Éducation",
+        categorie: "Sciences de l'éducation",
+        fichier: "content/cours/introduction-aux-sciences-de-l-éducation.md"
+    }
+];
+
+
+const liste = document.getElementById("liste-cours");
+
+
+liste.innerHTML = "";
+
+
+cours.forEach(c => {
+
+    let bloc = document.createElement("article");
+
+
+    bloc.innerHTML = `
+
+        <h3>${c.titre}</h3>
+
+        <p>
+        Catégorie : ${c.categorie}
+        </p>
+
+        <a href="${c.fichier}">
+        Lire le cours
+        </a>
+
+    `;
+
+
+    liste.appendChild(bloc);
+
+});
